51326734207e6559db24ea981d00ee91ed9fbda6
Masafumi Yokoyama <myokoym@gmail.com>
2013-06-29 00:03:38 +0900

    Add test

---
+++ test/test-pic.rb
@@ -0,0 +1,12 @@
+require "pix_scale/pic"
+
+class PicTest < Test::Unit::TestCase
+  def test_scale_and_save
+    path = "test/fixtures/nijip.png"
+    scaled_path = "test/fixtures/nijip-0.5.png"
+    PixScale::Pic.scale_and_save(path, 0.5)
+    assert_true(File.file?(scaled_path))
+    assert_true(File.size(path) > File.size(scaled_path))
+    File.delete(scaled_path)
+  end
+end
