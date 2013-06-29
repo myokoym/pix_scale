0b54d810fbf5f4b2897d821bb84ef3729c77e60b
Masafumi Yokoyama <myokoym@gmail.com>
2013-06-29 13:23:24 +0900

    Use an instance variable

--- lib/pix_scale/pic.rb
+++ lib/pix_scale/pic.rb
@@ -6,20 +6,20 @@ module PixScale
       new(path).scale_and_save(scale)
     end
 
-    attr_reader :path, :dirname, :basename, :extname, :pic
+    attr_reader :path, :dirname, :basename, :extname, :pic, :type
     def initialize(path)
       @path = path
       @dirname  = File.dirname(path)
       @basename = File.basename(path, ".*")
       @extname  = File.extname(path).sub(/^\./, "")
       @pic = Gdk::Pixbuf.new(path)
+      @type = (/\A\.jpg\z/ =~ extname) ? "jpeg" : extname
     end
 
     def scale_and_save(scale)
       scaled_pic = pic.scale(pic.width * scale, pic.height * scale)
 
       output_path = "#{dirname}/#{basename}-#{scale.to_s}.#{extname}"
-      type = (/\Ajpg\z/ =~ extname) ? "jpeg" : extname
       scaled_pic.save(output_path, type)
     end
   end
