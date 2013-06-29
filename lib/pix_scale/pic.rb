5a2bf2fb55009237c6f538d8a99046d2504ad346
Masafumi Yokoyama <myokoym@gmail.com>
2013-06-29 12:57:43 +0900

    Support instantiation

--- lib/pix_scale/pic.rb
+++ lib/pix_scale/pic.rb
@@ -3,11 +3,19 @@ require "gdk_pixbuf2"
 module PixScale
   class Pic
     def self.scale_and_save(path, scale)
-      dirname  = File.dirname(path)
-      basename = File.basename(path, ".*")
-      extname  = File.extname(path).sub(/^\./, "")
+      new(path).scale_and_save(scale)
+    end
 
-      pic = Gdk::Pixbuf.new(path)
+    def initialize(path)
+      @path = path
+    end
+
+    def scale_and_save(scale)
+      dirname  = File.dirname(@path)
+      basename = File.basename(@path, ".*")
+      extname  = File.extname(@path).sub(/^\./, "")
+
+      pic = Gdk::Pixbuf.new(@path)
       scaled_pic = pic.scale(pic.width * scale, pic.height * scale)
 
       output_path = "#{dirname}/#{basename}-#{scale.to_s}.#{extname}"
--- test/test-pic.rb
+++ test/test-pic.rb
@@ -1,11 +1,23 @@
 require "pix_scale/pic"
 
 class PicTest < Test::Unit::TestCase
-  def test_scale_and_save
+  def test_static_scale_and_save
     path = "test/fixtures/nijip.png"
     scaled_path = "test/fixtures/nijip-0.5.png"
     File.delete(scaled_path) if File.exist?(scaled_path)
     PixScale::Pic.scale_and_save(path, 0.5)
+    assert_true(File.file?(scaled_path))
+    assert_true(File.size(path) > File.size(scaled_path))
+    assert_true(File.size(scaled_path) > 0)
+    File.delete(scaled_path)
+  end
+
+  def test_scale_and_save
+    path = "test/fixtures/nijip.png"
+    scaled_path = "test/fixtures/nijip-0.5.png"
+    File.delete(scaled_path) if File.exist?(scaled_path)
+    pic = PixScale::Pic.new(path)
+    pic.scale_and_save(0.5)
     assert_true(File.file?(scaled_path))
     assert_true(File.size(path) > File.size(scaled_path))
     assert_true(File.size(scaled_path) > 0)
