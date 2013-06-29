ab02bfab5d928e990af9125bde613b8a601b1098
Masafumi Yokoyama <myokoym@gmail.com>
2013-06-29 12:38:05 +0900

    test: add cleanup before calling the method for a test

--- test/test-pic.rb
+++ test/test-pic.rb
@@ -4,6 +4,7 @@ class PicTest < Test::Unit::TestCase
   def test_scale_and_save
     path = "test/fixtures/nijip.png"
     scaled_path = "test/fixtures/nijip-0.5.png"
+    File.delete(scaled_path) if File.exist?(scaled_path)
     PixScale::Pic.scale_and_save(path, 0.5)
     assert_true(File.file?(scaled_path))
     assert_true(File.size(path) > File.size(scaled_path))
